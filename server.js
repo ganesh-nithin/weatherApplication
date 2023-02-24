f9d561d834546a703130e95cbf4971f41957b3f9
Sarvabhotla G. Nithin <sarvabhotla.g@solitontech.com>
2023-02-24 11:37:52 +0530

    feat : JS : Access weather data using local host
    ID : T3001
    
    Access weather data using local host.
    
    Signed-off-by: Sarvabhotla G. Nithin <sarvabhotla.g@solitontech.com>

--- server.js
+++ server.js
@@ -1,41 +1,76 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const {
+  allTimeZones,
+  timeForOneCity,
+  nextNhoursWeather,
+} = require("./timeZone");
+
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  let filePath = path.join(__dirname, req.url === "/" ? "index.html" : req.url);
-  let extName = path.extname(filePath);
-  let contentType = "text/html";
+http
+  .createServer((req, res) => {
+    let filePath = path.join(
+      __dirname,
+      req.url === "/" ? "index.html" : req.url
+    );
+    let extName = path.extname(filePath);
+    let contentType = "text/html";
 
-  switch (extName) {
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".js":
-      contentType = "text/js";
-      break;
-    case ".svg":
-      contentType = "image/svg+xml";
-      break;
-    case ".ico":
-      contentType = "image/x-icon";
-      break;
-  }
+    switch (extName) {
+      case ".css":
+        contentType = "text/css";
+        break;
+      case ".js":
+        contentType = "text/js";
+        break;
+      case ".svg":
+        contentType = "image/svg+xml";
+        break;
+      case ".ico":
+        contentType = "image/x-icon";
+        break;
+    }
 
-  console.log(`File path: ${filePath}`);
-  console.log(`Content-Type: ${contentType}`);
-
-  res.writeHead(200, { "Content-Type": contentType });
-
-  const readStream = fs.createReadStream(filePath);
-  readStream.pipe(res);
-});
-
-server.listen(port, (err) => {
-  if (err) {
-    console.log(`Error: ${err}`);
-  } else {
-    console.log(`Server listening at port ${port}...`);
-  }
-});
+    if (req.url === "/allTimeZones") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      const jsonContent = JSON.stringify(allTimeZones());
+      res.write(jsonContent);
+      res.end();
+    } else if (req.url.startsWith("/timeForOneCity")) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      let cityName = req.url.split("=")[1];
+      const jsonContent = JSON.stringify(timeForOneCity(cityName));
+      res.write(jsonContent);
+      res.end();
+    } else if (req.url === "/nextNhoursWeather") {
+      let data = "";
+      req.on("data", function (chunk) {
+        data += chunk;
+      });
+      req.on("end", () => {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        data = JSON.parse(data);
+        let content = nextNhoursWeather(
+          data.city_Date_Time_Name,
+          data.hours,
+          allTimeZones()
+        );
+        const jsonContent = JSON.stringify(content);
+        res.write(jsonContent);
+        res.end();
+      });
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      const readStream = fs.createReadStream(filePath);
+      readStream.pipe(res);
+    }
+  })
+  .listen(port, (err) => {
+    if (err) {
+      console.log(`Error: ${err}`);
+    } else {
+      console.log(`Server listening at port ${port}...`);
+    }
+  });
