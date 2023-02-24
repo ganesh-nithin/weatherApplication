fc4e08f33f3e60935296af0d4a11f2579e718f44
Sarvabhotla G. Nithin <sarvabhotla.g@solitontech.com>
2023-02-24 19:52:36 +0530

    feat : JS : Get allTimeZoneData
    ID : T2003
    
    Get allTimeZoneData
    
    Signed-off-by: Sarvabhotla G. Nithin <sarvabhotla.g@solitontech.com>

--- server.js
+++ server.js
@@ -11,33 +11,44 @@ app.use(express.static("public"));
 app.use(express.json());
 app.listen(port);
 
-    switch (extensionName) {
-      case ".css":
-        contentType = "text/css";
-        break;
-      case ".js":
-        contentType = "text/js";
-        break;
-      case ".svg":
-        contentType = "image/svg+xml";
-        break;
-      case ".ico":
-        contentType = "image/x-icon";
-        break;
-    }
+app.get("/allTimeZones", (request, response) => {
+  let allTimeZonesData = allTimeZones();
 
-    switch (request.url) {
-      case "/allTimeZones":
-        writeAllTimeZoneData(response);
-        break;
-      case request.url.startsWith("/timeForOneCity") ? request.url : "":
-        writeTimeForOneCityData(request, response);
-        break;
-      case "/nextNhoursWeather":
-        writeNextNhoursWeatherData(request, response);
-        break;
-      default:
-        write(response, contentType, filePath);
+  response.json(allTimeZonesData);
+});
+
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.write(allTimeZonesData);
+      response.end();
+    } else if (request.url.startsWith("/timeForOneCity")) {
+      let cityName = request.url.split("=")[1];
+      let timeForOneCityData = JSON.stringify(timeForOneCity(cityName));
+
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.write(timeForOneCityData);
+      response.end();
+    } else if (request.url === "/nextNhoursWeather") {
+      let timeForOneCityData = "";
+
+      request.on("data", function (chunk) {
+        timeForOneCityData += chunk;
+      });
+      request.on("end", () => {
+        response.writeHead(200, { "Content-Type": "application/json" });
+        timeForOneCityData = JSON.parse(timeForOneCityData);
+        let nextNhoursWeatherData = nextNhoursWeather(
+          timeForOneCityData.city_Date_Time_Name,
+          timeForOneCityData.hours,
+          allTimeZones()
+        );
+        nextNhoursWeatherData = JSON.stringify(nextNhoursWeatherData);
+        response.write(nextNhoursWeatherData);
+        response.end();
+      });
+    } else {
+      response.writeHead(200, { "Content-Type": contentType });
+      const readStream = fs.createReadStream(filePath);
+      readStream.pipe(response);
     }
   })
   .listen(port, (err) => {
